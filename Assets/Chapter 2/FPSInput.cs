34bc4fad5822ba1fc86c0112441e533aa1f9d25b
Mark Vo <Mark.Vo001@umb.edu>
2018-04-23 20:42:11 -0400

    Finished Chapter 2
    Scripts for moving rotating fps camera through mouse, and character
    movement through keyboard.

---
+++ Assets/Chapter 2/FPSInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class FPSInput : MonoBehaviour {
+
+    public float gravity = -9.8f;
+    public float speed = 6.0f;
+    private CharacterController _charController;
+
+    // Use this for initialization
+    void Start () {
+		_charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed); // limit magnitude to movement speed
+        movement.y = gravity;
+        movement *= Time.deltaTime;                         // scale per frame
+        movement = transform.TransformDirection(movement);  // convert from local to global space   
+        _charController.Move(movement); // tell character to move by that vector
+
+    }
+}
---
+++ Assets/Chapter 2/MouseLook.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVer = 9.0f;
+
+    public float miniimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+	
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if(body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+        if(axes == RotationAxes.MouseX)
+        {
+            // horizontal rotation here
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if(axes == RotationAxes.MouseY)
+        {
+            // vertical rotation here
+            // Instead of using transform.Rotate, it is assigning the new rotation directly after calculating 
+            // the new angle and then creating a new vector and then assigning it because we are limiting how far it can rotate vertically.
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;   
+            _rotationX = Mathf.Clamp(_rotationX, miniimumVert, maximumVert);    // Limit the rotation between max and min
+
+            float rotationY = transform.localEulerAngles.y;                     // Get the same horizontal position
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0); // Assign its new rotation directly
+        }
+        else
+        {
+            // both horizontal and vertical rotation here
+
+            // Calculating vertical movement here
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
+            _rotationX = Mathf.Clamp(_rotationX, miniimumVert, maximumVert);
+
+            // Calculating the horizontal movement here
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+
+
+
+            //transform.Rotate(0, 0, 0);
+        }
+
+
+	}
+}

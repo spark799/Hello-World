a4a926cf542fe98ff78e60a4aae7ce2deb5061f7
Mark Vo <markvo799@gmail.com>
2018-05-04 22:01:22 -0400

    Creating interactable UI

---
+++ Assets/Chapter 6/Scripts/SettingsPopUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPopUp : MonoBehaviour {
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Chapter 6/Scripts/UIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField] private Text scoreLabel;
+    [SerializeField] private SettingsPopUp settingsPopup;
+
+
+    private void Start()
+    {
+        settingsPopup.Close();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+
+    }
+
+    public void OpenSettings()
+    {
+        settingsPopup.Open();
+
+
+    }
+
+    public void OnPointerDown()
+    {
+        Debug.Log("pointer down");
+    }
+}
